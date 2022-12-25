360b13dde78c5128159bb3c5a6eead2ed341075c
Sedtanan <setthanan2544@gmail.com>
2022-12-25 11:08:08 +0700

    lab2

---
+++ lab02-2022-12-22/as01/target/debug/deps/as01.d
@@ -0,0 +1,5 @@
+C:\Work\mobilecloud\Rust_project\kmutnb-cn-sedtanan-6303051623144\lab02-2022-12-22\as01\target\debug\deps\as01.exe: src\main.rs
+
+C:\Work\mobilecloud\Rust_project\kmutnb-cn-sedtanan-6303051623144\lab02-2022-12-22\as01\target\debug\deps\as01.d: src\main.rs
+
+src\main.rs:
